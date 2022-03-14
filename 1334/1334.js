c2e1430167310145e4fd51e62b708a0e37153634
Yjs9 <89894532+Yjs9@users.noreply.github.com>
2022-03-14 16:14:55 +0900

    길이가 홀수인 경우 완료

--- 1334/1334.js
+++ 1334/1334.js
@@ -5,35 +5,76 @@ let input = fs.readFileSync(filePath).toString().trim();
 const size = input.length;
 if (size % 2 == 0) {
   //짝수일 때
-  const 짝수 = input.substring(parseInt(size / 2));
-  const 리버스짝수 = zak.split("").reverse().join("");
-  console.log(짝수);
-  console.log(리버스짝수 + 짝수);
+  const left = input.substring(0, parseInt(size / 2 + 1) - 1);
+  const right = input.substring(input.length, parseInt(size / 2));
+  const 왼쪽이작을때 = left.split("").reverse().join("");
+  const 오른쪽이작을때 = right.split("").reverse().join("");
+  console.log(left, right, 왼쪽이작을때, 오른쪽이작을때);
+  let 구 = "";
+  for (i = 0; i < input.length; i++) {
+    구 += "9";
+  }
+  if (input == 구) {
+    console.log(String(parseInt(구) + 2));
+  }
 } else {
   //홀수일 때
   const left = input.substring(0, parseInt(size / 2 + 1) - 1);
   const right = input.substring(input.length, parseInt(size / 2 + 1));
   const 왼쪽이작을때 = left.split("").reverse().join("");
-  console.log(left, right, 왼쪽이작을때);
   const 오른쪽이작을때 = right.split("").reverse().join("");
-  if (left < right) {
-    const 왼쪽 = input.substring(0, parseInt(size / 2 + 1));
-    const 팰린드롬 = 왼쪽 + 왼쪽이작을때;
-    console.log(왼쪽, 팰린드롬);
-    if (parseInt(팰린드롬) < parseInt(input)) {
-      const left = input.substring(0, parseInt(size / 2 + 1) - 1);
+  console.log(left, right, 왼쪽이작을때, 오른쪽이작을때);
+  let 구 = "";
+  for (i = 0; i < input.length; i++) {
+    구 += "9";
+  }
+  if (input == 구) {
+    console.log(String(parseInt(구) + 2));
+  } else {
+    if (left < right) {
+      // 왼쪽이 작을때
+      const 왼쪽 = input.substring(0, parseInt(size / 2 + 1));
+      const 팰린드롬 = 왼쪽 + 왼쪽이작을때;
+      if (parseInt(팰린드롬) <= parseInt(input)) {
+        // 완성된 팰린드롬이 제시된 수 보다 큰지 확인
+        const left = input.substring(0, parseInt(size / 2 + 1) - 1);
+        const 중간수 = input.substring(input.length / 2, input.length / 2 + 1);
+        if (중간수 == 9) {
+          const 구팰린드롬 =
+            parseInt(left) +
+            1 +
+            "0" +
+            String(parseInt(left) + 1)
+              .split("")
+              .reverse()
+              .join("");
+          console.log(구팰린드롬);
+        } else {
+          const 다음팰린드롬 = left + (parseInt(중간수) + 1) + 왼쪽이작을때;
+          console.log(다음팰린드롬);
+        }
+      }
+    } else if (left === right) {
       const 중간수 = input.substring(input.length / 2, input.length / 2 + 1);
-      const 다음팰린드롬 = left + (parseInt(중간수) + 1) + 왼쪽이작을때;
-      console.log(left, 중간수, 다음팰린드롬);
-    } else {
+      if (중간수 == 9) {
+        const 구팰린드롬 =
+          parseInt(left) +
+          1 +
+          "0" +
+          String(parseInt(left) + 1)
+            .split("")
+            .reverse()
+            .join("");
+        console.log(구팰린드롬);
+      } else {
+        const 다음팰린드롬 = left + (parseInt(중간수) + 1) + 왼쪽이작을때;
+        console.log(다음팰린드롬);
+      }
+    } else if (left > right) {
+      // 오른쪽이 작을때
+      const 왼쪽 = input.substring(0, parseInt(size / 2 + 1));
+      const 팰린드롬 = 왼쪽 + 왼쪽이작을때;
       console.log(팰린드롬);
     }
-  } else if (left === right) {
-    const 중간수 = input.substring(input.length / 2, input.length / 2 + 1);
-    const 변경할수 = input.substring(input.length, parseInt(size / 2 + 1));
-    console.log(변경할수);
-    const 팰린드롬 = input.replace(변경할수, 왼쪽이작을때);
-    const 다음팰린드롬 = 팰린드롬.replace(중간수, parseInt(중간수) + 1);
-    console.log(다음팰린드롬);
   }
 }
