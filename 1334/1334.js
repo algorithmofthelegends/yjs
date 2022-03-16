925e4800e63e22603cb20c84be19a26c0f0b2d33
Yjs9 <89894532+Yjs9@users.noreply.github.com>
2022-03-16 14:17:49 +0900

    완성

--- 1334/1334.js
+++ 1334/1334.js
@@ -1,5 +1,5 @@
+//const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 const fs = require("fs");
-//const filePath = "/dev/stdin";
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim();
 const size = input.length;
@@ -13,30 +13,37 @@ if (size % 2 == 0) {
     nine += "9";
   }
   if (input == nine) {
-    console.log(String(parseInt(nine) + 2));
+    console.log(String(BigInt(nine) + 2n));
   } else {
-    if (left < right) {
+    if ((left < right) | (left > right)) {
       const p = left + rleft;
       if (p <= input) {
         const np =
-          parseInt(left) +
-          1 +
-          String(parseInt(left) + 1)
+          BigInt(left) +
+          1n +
+          String(BigInt(left) + 1n)
             .split("")
             .reverse()
             .join("");
         console.log(np);
+      } else {
+        console.log(p);
       }
     } else if (left === right) {
-      const np =
-        parseInt(left) +
-        1 +
-        String(parseInt(left) + 1)
-          .split("")
-          .reverse()
-          .join("");
-      console.log(np);
-    } else if (left > right) {
+      const p = left + rleft;
+      if (input >= p) {
+        const np =
+          BigInt(left) +
+          1n +
+          String(BigInt(left) + 1n)
+            .split("")
+            .reverse()
+            .join("");
+        console.log(np);
+      } else {
+        console.log(p);
+      }
+    } else {
       const p = left + rleft;
       console.log(p);
     }
@@ -51,9 +58,9 @@ if (size % 2 == 0) {
     nine += "9";
   }
   if (input == nine) {
-    console.log(String(parseInt(nine) + 2));
+    console.log(String(BigInt(nine) + 2n));
   } else {
-    if (left < right) {
+    if ((left < right) | (left > right)) {
       // 왼쪽이 작을때
       const ll = input.substring(0, parseInt(size / 2 + 1));
       const p = ll + rleft;
@@ -63,10 +70,10 @@ if (size % 2 == 0) {
         const mid = input.substring(input.length / 2, input.length / 2 + 1);
         if (mid == 9) {
           const np =
-            parseInt(left) +
-            1 +
+            BigInt(left) +
+            1n +
             "0" +
-            String(parseInt(left) + 1)
+            String(BigInt(left) + 1n)
               .split("")
               .reverse()
               .join("");
@@ -75,27 +82,33 @@ if (size % 2 == 0) {
           const nep = left + (parseInt(mid) + 1) + rleft;
           console.log(nep);
         }
+      } else {
+        console.log(p);
       }
     } else if (left === right) {
-      const mid = input.substring(input.length / 2, input.length / 2 + 1);
-      if (mid == 9) {
-        const np =
-          parseInt(left) +
-          1 +
-          "0" +
-          String(parseInt(left) + 1)
-            .split("")
-            .reverse()
-            .join("");
-        console.log(np);
+      const ll = input.substring(0, parseInt(size / 2 + 1));
+      const p = ll + rleft;
+      if (input >= p) {
+        const mid = input.substring(input.length / 2, input.length / 2 + 1);
+        if (mid == 9) {
+          const np =
+            BigInt(left) +
+            1n +
+            "0" +
+            String(BigInt(left) + 1n)
+              .split("")
+              .reverse()
+              .join("");
+          console.log(np);
+        } else {
+          const nep = left + (parseInt(mid) + 1) + rleft;
+          console.log(nep);
+        }
       } else {
-        const nep = left + (parseInt(mid) + 1) + rleft;
-        console.log(nep);
+        console.log(p);
       }
-    } else if (left > right) {
-      // 오른쪽이 작을때
-      const l = input.substring(0, parseInt(size / 2 + 1));
-      const p = l + rleft;
+    } else {
+      const p = left + rleft;
       console.log(p);
     }
   }
