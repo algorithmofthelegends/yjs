779127e1d883675b5ef3cc57e0013ca15517574d
Yjs9 <89894532+Yjs9@users.noreply.github.com>
2022-03-17 12:00:32 +0900

    .

--- 1334/1427.js
+++ 1334/1427.js
@@ -1,9 +1,9 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim();
-// const fs = require("fs");
-// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-// let input = fs.readFileSync(filePath).toString().trim();
+//const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
 
-const inputArr = Array.from(input);
-const sortInput = inputArr.sort((a, b) => b - a);
-const result = sortInput.join("");
+const result = Array.from(input)
+  .sort((a, b) => b - a)
+  .join("");
 console.log(result);
